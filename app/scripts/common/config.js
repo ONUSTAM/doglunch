df2a3c34c12efff3845273cbbdc9bc5aa936d8b0
matsunoTakuma <matsuno@16bit.co.jp>
2015-12-19 17:19:33 +0900

    workflow

---
+++ app/scripts/common/config.js
@@ -0,0 +1,15 @@
+var config = {
+  api : {
+    type : {
+      post : 'POST',
+      get : 'GET'
+    },
+    method: {
+      doglunch: 'xxxxx'
+    }
+  },
+  pagepath : {
+    // index.html
+    front : '^/$'
+  }
+};
